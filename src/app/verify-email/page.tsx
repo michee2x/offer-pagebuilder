ca4f61b0d63cc8621c8e6be63301cb6828e3c50f
michee2x <michaelisraelmike@gmail.com>
2026-04-30 19:19:37 +0100

    latest

--- src/app/verify-email/page.tsx
+++ src/app/verify-email/page.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import { createClient } from "@/utils/supabase/client";
 import { useRouter, useSearchParams } from "next/navigation";
 
-export const dynamic = "force-dynamic";
+export const runtime = "edge";
 
 export default function VerifyEmailPage() {
   const [email, setEmail] = useState("");
